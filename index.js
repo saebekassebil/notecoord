ce1dfb2f2db4ad49e5a5a6061554911b454cfbc8
Jakob Miland <saebekassebil@gmail.com>
2014-07-15 19:07:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// First coord is octaves, second is fifths. Distances are relative to c
+var notes = {
+  c: [0, 0],
+  d: [-1, 2],
+  e: [-2, 4],
+  f: [1, -1],
+  g: [0, 1],
+  a: [-1, 3],
+  b: [-2, 5],
+  h: [-2, 5]
+};
+
+module.exports = function(name) {
+  return notes[name];
+};
+
+module.exports.notes = notes;
+module.exports.A4 = [3, 3]; // Relative to C0 (scientic notation, ~16.35Hz)
